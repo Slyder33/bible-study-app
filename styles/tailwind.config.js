ef2577fd541a594af28373557d9fa8725de16e43
Slyder33 <Slyder33@gmail.com>
2025-07-15 17:08:11 -0500

    Update tailwind.config.js

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  darkMode: 'class', // enables “class” strategy
+  darkMode: 'class',
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  safelist: [
+    'bg-yellow-200',
+    'dark:bg-yellow-800',
+  ],
   theme: { extend: {} },
   plugins: [],
 };
